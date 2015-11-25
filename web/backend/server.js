e58a572718418a2df76169374e544cbb365231ec
Benjamin Fleckenstein <bf@nbkr.net>
2015-11-25 18:43:05 +0100

    The Web Backend now has topic control inplace

--- web/backend/server.js
+++ web/backend/server.js
@@ -1,24 +1,56 @@
 var io = require('socket.io')(8080);
 var mqtt    = require('mqtt');
 var client  = mqtt.connect({ port: 1883, host: 'localhost'});
+var fs = require('fs');
 
  
 client.on('connect', function () {
-  client.subscribe('tfin1/port0');
-  client.subscribe('tfout1/port0');
+  // Reading the topics we should subscribe to.
+  fs.readFile('subscriptions.txt', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var lines = data.trim().split("\n")
+    for (var i = 0; i < lines.length; i++) {
+        console.log('Subscribing to ' + lines[i].trim());
+        client.subscribe(lines[i].trim());
+    }
+  });
 });
  
 client.on('message', function (topic, message) {
-  // message is Buffer 
-  console.log(topic);
   io.sockets.emit(topic, message.toString());
 });
 
 io.on('connection', function(socket) {
     console.log('New Connection');
-    socket.on('publish', function(data) {
-        topic = data['topic'];
-        message = data['message'];
-        client.publish(topic, message);
+
+    // Reading the allowd forwardings. I'm not sure 
+    // if this is a good idea to do this here. 
+    //
+    // On the one hand
+    // a change in 'forwardings.txt' doesn't require a restart,
+    // just a reload from the browser.
+    //
+    // On the other hand now the file is read everytime a bowser 
+    // connects.
+    fs.readFile('forwardings.txt', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+
+        lines = data.trim().split("\n")
+        for (var i = 0; i < lines.length; i++) {
+            var topic = lines[i].trim()
+            console.log('Adding socket.io forwarding: ' + topic);
+            socket.on(topic, function(data) {
+                console.log('publishing ' + topic + '/' + data); 
+                client.publish(topic, data);
+            });
+        }
     });
+    
 });
+
+
