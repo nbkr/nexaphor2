a47ea6c412e895f839a5fe850beff0b23fec0c1e
Benjamin Fleckenstein <bf@nbkr.net>
2015-12-02 07:47:06 +0100

    Troubleshooting

--- web/frontend/js/main.js
+++ web/frontend/js/main.js
@@ -25,8 +25,7 @@ function message(topic, message) {
     }
 
     var c = null;
-    console.log(topics2components);
-    console.log(topic);
+    console.log(topics2components[topic]);
     for (var i = 0; topics2components[topic].length; i++) {
         c = topics2components[topic][i];
         c['message'](topic, message);
